a65434b91910690de8ba8188d5b4d681f4244cf7
Christian Parker <christianparker512@gmail.com>
2021-01-11 16:59:06 -0600

    polymorphism

---
+++ src/BillsBurgers/DeluxeHamburger.java
@@ -0,0 +1,8 @@
+package BillsBurgers;
+
+//public class DeluxeHamburger extends Hamburger{
+//    public DeluxeHamburger(){
+//        super("Deluxe", "Brisket, Wagyu Beef, and Bacon", 14.99, "ciabatta");
+//        super.add
+//    }
+//}
--- src/BillsBurgers/Main.java
+++ src/BillsBurgers/Main.java
@@ -5,10 +5,14 @@ public class Main {
     Hamburger hamburger = new Hamburger ("Basic", "beef", 7.99, "Sesame");
         System.out.println(hamburger);
     double price = hamburger.itemizeHamburger();
+        System.out.println("You just ordered a a new burgers");
     hamburger.addHamburgerAddition1("cheese", .50);
     hamburger.addHamburgerAddition2("grilled onions", .75);
     hamburger.addHamburgerAddition3("avocado", .99);
         System.out.println("The total price of the burger is " + hamburger.itemizeHamburger());
+    HealthyBurger healthyBurger = new HealthyBurger("Turkey", 8.99);
+    healthyBurger.addHamburgerAddition1("avocado", .99);
+    healthyBurger.addHamburgerAddition2("havarti", .50);
     }
 
 }
