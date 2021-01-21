e02adf4f6045d97bd5ac97fe2cd37ae6a978bac3
Christian Parker <christianparker512@gmail.com>
2021-01-21 07:17:00 -0600

    using the for loop to fill the array

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -3,6 +3,15 @@ package Arrays;
 public class Main {
     public static void main(String[] args) {
         int []  myIntArray = new int[10];
-        myIntArray [5] = 50;
+//        int [] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+//        myIntArray[0]= 45;
+//        myIntArray[1] = 476;
+//        myIntArray [5] = 50;
+//        double [] myDoubleArray  = new double [10];
+        for (int i =0; i<10; i++){
+            myIntArray[i] = i * 10;
+        }
+        System.out.println(myIntArray[5]);
+        System.out.println(myIntArray[0]);
     }
 }
