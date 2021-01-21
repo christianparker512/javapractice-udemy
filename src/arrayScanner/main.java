44e4aeba621c5a33a73fa880178b70ca7b7791c3
Christian Parker <christianparker512@gmail.com>
2021-01-21 07:37:51 -0600

    use of scanner to create an array and then return the values in a forloop

---
+++ src/arrayScanner/main.java
@@ -0,0 +1,23 @@
+package arrayScanner;
+
+import java.util.Scanner;
+
+public class main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for (int i =0; i<myIntegers.length; i++){
+            System.out.println("Element " + i + " typed value was "+ myIntegers[i]);
+        }
+
+    }
+    public static int[] getIntegers(int number){
+        System.out.println("enter "+ number + " integer values. \r");
+        int[] values = new int [number];
+        for (int i =0; i<values.length; i++){
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+}
