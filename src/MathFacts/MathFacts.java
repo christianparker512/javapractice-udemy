0e8da8fc093d7d98914324132a0ba9d7885a4b82
Christian Parker <christianparker512@gmail.com>
2021-01-24 12:18:24 -0600

    multiplication

--- src/MathFacts/MathFacts.java
+++ src/MathFacts/MathFacts.java
@@ -9,12 +9,16 @@ public class MathFacts {
     public static int minus (int a, int b){
         return a - b;
     }
+    public static int times (int a, int b){
+        return a * b;
+    }
 
 
     public static void main(String[] args) {
         MathFacts addition = new MathFacts();
         System.out.println(add(5,5));
         System.out.println(minus(10,5));
+        System.out.println(times(5, 5));
 
     }
 }
