6dd941afc79367a92bcc4a78a55f664fa4a94665
Christian Parker <christianparker512@gmail.com>
2021-01-24 12:17:47 -0600

    Math facts udemy

---
+++ src/MathFacts/MathFacts.java
@@ -0,0 +1,20 @@
+package MathFacts;
+
+public class MathFacts {
+    public MathFacts (){}
+
+    public static int add (int a, int b){
+        return a + b;
+    }
+    public static int minus (int a, int b){
+        return a - b;
+    }
+
+
+    public static void main(String[] args) {
+        MathFacts addition = new MathFacts();
+        System.out.println(add(5,5));
+        System.out.println(minus(10,5));
+
+    }
+}
