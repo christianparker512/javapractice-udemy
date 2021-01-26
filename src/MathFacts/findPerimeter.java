720490f9ca85ef15958a601c34fb9cc2444036f5
Christian Parker <christianparker512@gmail.com>
2021-01-26 12:20:39 -0600

    findPerimeter

---
+++ src/MathFacts/findPerimeter.java
@@ -0,0 +1,12 @@
+package MathFacts;
+
+
+public class findPerimeter{
+    public static int findPerimeter(int length, int width) {
+        return 2 * length + 2 * width;
+    }
+    public static void main(String[] args) {
+        System.out.println(findPerimeter(4,5));
+    }
+}
+
