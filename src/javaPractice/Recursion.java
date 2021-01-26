c366d38b4febddda398b296653f7d82ce49da240
Christian Parker <christianparker512@gmail.com>
2021-01-26 08:35:33 -0600

    recursion example

--- src/javaPractice/Recursion.java
+++ src/javaPractice/Recursion.java
@@ -9,7 +9,16 @@ public class Recursion {
         System.out.println(n);
         count(n-1);
     }
+    public static long getPower(int base, int exponent) {
+        long result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result = result * base;
+        }
+        System.out.println(result);
+        return result;
+    }
     public static void main(String[] args) {
         count(5);
+        getPower(5,5);
     }
 }
